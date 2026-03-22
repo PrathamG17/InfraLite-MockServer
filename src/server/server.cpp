73327b85e77bd428cc406109a3e9069719265420
Prathamesh-Gaikwad <prathamesh.b.g2002@gmail.com>
2026-03-22 20:38:10 +0530

    ci updated

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -118,7 +118,7 @@ void CServer::InitSocket()
         WSACleanup();
         throw std::runtime_error("listen failed");
     }
-    std::cout << "Server listening on http://localhost:8080" << std::endl;
+    std::cout << "Server listening on https://localhost:8080" << std::endl;
 }
 
 int CServer::AcceptClient() const
